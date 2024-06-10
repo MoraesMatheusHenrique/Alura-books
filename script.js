1328a0f11bb296179c04421f72276f726c9faebf
MoraesMatheusHenrique <110630942+MoraesMatheusHenrique@users.noreply.github.com>
2024-06-10 10:36:01 -0300

    aula 5

--- script.js
+++ script.js
@@ -46,8 +46,18 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContet = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
 mostraPergunta();
+
